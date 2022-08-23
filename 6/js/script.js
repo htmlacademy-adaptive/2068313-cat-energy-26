e651c8378f16dbe7dc3bbb9debecefeb30d3f341
Keks <keks@htmlacademy.ru>
2022-08-23 21:13:47 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/script.js
@@ -0,0 +1,16 @@
+const btnNav = document.querySelector(".nav__toggle");
+const wrapMenu = document.querySelector(".nav__wrapper");
+const classMenuActive = "nav__wrapper-active";
+
+const openMenu = ()=> {
+  wrapMenu.classList.toggle(classMenuActive);
+
+  if(wrapMenu.classList.contains(classMenuActive)){
+    btnNav.style.backgroundImage = "url(./img/icons/Logo_mobile-close.svg)";
+
+  }else{
+    btnNav.style.backgroundImage = "url(./img/icons/Logo_mobile.svg)";
+  }
+}
+
+btnNav.addEventListener('click', openMenu);
